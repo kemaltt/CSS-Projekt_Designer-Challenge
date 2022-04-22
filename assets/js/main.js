ff69fedbc80fefdbc69307c572cdaa6f3f992488
kemaltt <ttnckemal@gmail.com>
2022-04-22 23:00:16 +0200

    modal styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const email = document.getElementById('input-email')
+const visible = document.getElementById('visible')
 const emailOutput = document.getElementById('email-output')
 const hiddenModal = document.getElementById('hidden-modal')
 
@@ -6,18 +7,18 @@ const hiddenModal = document.getElementById('hidden-modal')
 const subscribeModal = () => {
 
     if (email.value == '') {
-        alert('enter a email')
+        alert('Please enter a Email adresse')
     } else {
         console.log(email.value);
         emailOutput.innerHTML = email.value
         hiddenModal.style.visibility = 'visible'
-        email.style.visibility = 'hidden'
+        visible.style.visibility = 'hidden'
     }
 
 }
 
 const closeModal = () => {
     hiddenModal.style.visibility = 'hidden'
-    email.style.visibility = 'visible'
+    visible.style.visibility = 'visible'
     email.value = ''
 }
